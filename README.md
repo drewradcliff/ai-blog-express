596019a8eab10bee0bf3c157f59f62d09bbc21c8
drew <sirampr@gmail.com>
2022-12-31 00:44:27 -0500

    Add post endpoint

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
-# ExpressJS Example
+# AI blog API
 
-This example starts an [ExpressJS](https://expressjs.com/) server written in [TypeScript](https://www.typescriptlang.org/).
-
-[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Frailwayapp-starters%2Fexpressjs)
-
-## ✨ Features
-
-- Express
-- TypeScript
-
-## 💁‍♀️ How to use
+## Gettings started
 
 - Install dependencies `pnpm install`
 - Connect to your Railway project `railway link`
 - Start the development server `railway run pnpm dev`
-
-## 📝 Notes
-
-The server started simply returns a `body: "Hello world!"` payload in JSON. The server code is located in `src/index.ts`.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import moment from "moment";
+import { openai } from "./lib/openai";
+import { twitterClient } from "./lib/twitter";
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -7,8 +10,34 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  res.json({ body: "Hello world!" });
+app.get("/post", async (req, res) => {
+  const { data } = await twitterClient.tweets.usersIdTweets("44196397", {
+    exclude: ["replies", "retweets"],
+    start_time: moment().subtract(1, "day").format(),
+    max_results: 100,
+    "tweet.fields": ["public_metrics"],
+  });
+
+  if (data) {
+    const topTweet = data?.reduce((prev, curr) =>
+      prev.public_metrics!.like_count > curr.public_metrics!.like_count
+        ? prev
+        : curr
+    );
+
+    const { data: openAiData } = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Write a blog post in html about '${topTweet?.text}' as if it was written by Elon Musk`,
+      max_tokens: 2000,
+      temperature: 0,
+    });
+    res.status(200).json({
+      tweet: topTweet,
+      data: openAiData,
+    });
+  } else {
+    return res.status(400).json("No tweets available today");
+  }
 });
 
 app.listen(port, () => {
