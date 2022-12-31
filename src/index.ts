9d4fdf7d1e95df0b9a5e2f25af6b483d6c271050
drew <sirampr@gmail.com>
2022-12-31 00:52:42 -0500

    Add authorization

--- src/index.ts
+++ src/index.ts
@@ -11,32 +11,40 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/post", async (req, res) => {
-  const { data } = await twitterClient.tweets.usersIdTweets("44196397", {
-    exclude: ["replies", "retweets"],
-    start_time: moment().subtract(1, "day").format(),
-    max_results: 100,
-    "tweet.fields": ["public_metrics"],
-  });
-
-  if (data) {
-    const topTweet = data?.reduce((prev, curr) =>
-      prev.public_metrics!.like_count > curr.public_metrics!.like_count
-        ? prev
-        : curr
-    );
-
-    const { data: openAiData } = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Write a blog post in html about '${topTweet?.text}' as if it was written by Elon Musk`,
-      max_tokens: 2000,
-      temperature: 0,
+  const { authorization } = req.headers;
+  if (authorization !== `Bearer ${process.env.NEXT_API_KEY}`) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+  try {
+    const { data } = await twitterClient.tweets.usersIdTweets("44196397", {
+      exclude: ["replies", "retweets"],
+      start_time: moment().subtract(1, "day").format(),
+      max_results: 100,
+      "tweet.fields": ["public_metrics"],
     });
-    res.status(200).json({
-      tweet: topTweet,
-      data: openAiData,
-    });
-  } else {
-    return res.status(400).json("No tweets available today");
+
+    if (data) {
+      const topTweet = data?.reduce((prev, curr) =>
+        prev.public_metrics!.like_count > curr.public_metrics!.like_count
+          ? prev
+          : curr
+      );
+
+      const { data: openAiData } = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Write a blog post in html about '${topTweet?.text}' as if it was written by Elon Musk`,
+        max_tokens: 2000,
+        temperature: 0,
+      });
+      res.status(200).json({
+        tweet: topTweet,
+        data: openAiData,
+      });
+    } else {
+      return res.status(500).json("No tweets available today");
+    }
+  } catch (err) {
+    return res.status(500).send("Error generating post");
   }
 });
 
